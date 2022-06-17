42eed8e06ea43c1e2b6e7a095eb19cc9616e8fe8
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-06-17 12:28:43 +0100

    created a new project

---
+++ src/main/java/learn/freelance/FreelanceApplication.java
@@ -0,0 +1,14 @@
+package learn.freelance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelanceApplication.class, args);
+        System.out.println("Finished running application.");
+    }
+
+}
